dd87f567df1f97ec231bfd31d58ec5169ecd5de4
Giuseppe Ciccone <72206039+GiuseppeCiccone96@users.noreply.github.com>
2022-05-06 08:53:11 +0100

    generalisation

--- file_sorter_fluorescence.py
+++ file_sorter_fluorescence.py
@@ -7,6 +7,7 @@ import os
 from os import listdir
 from os.path import isfile, join
 
+
 #Path where images are saved (ie folder)
 path="/Users/giuseppeciccone/Desktop/Pos0/" 
 #Name of the sample imaged
@@ -23,7 +24,7 @@ phall = files[0::3] #every thrird element starting from 0 is phalloidin
 yap = files[1::3] #every thrird element starting from 1 is yap
 dapi = files[2::3] #every thrird element starting from 2 is dapi
 
-#renaming Phall
+#renaming channel 1
 for index, filename in enumerate(phall): 
         if filename.endswith(".tif"):
                 my_dest = condition + channel_1  + str(index) + ".tif"
@@ -33,7 +34,7 @@ for index, filename in enumerate(phall):
                 # rename all the files
                 os.rename(my_source, my_dest)
 
-#Renaming Yap
+#renaming channel 2
 for index, filename in enumerate(yap): 
         if filename.endswith(".tif"):
                 my_dest =condition + channel_2 + str(index) + ".tif"
@@ -43,7 +44,7 @@ for index, filename in enumerate(yap):
                 # rename all the files
                 os.rename(my_source, my_dest)
         
-#Renaming Dapi
+#renaming channel 3
 for index, filename in enumerate(dapi): 
         if filename.endswith(".tif"):
                 my_dest =condition + channel_3 + str(index) + ".tif"
